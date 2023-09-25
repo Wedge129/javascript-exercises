f995e03d3a99cadaf71d6d48f9691a4af07f4b0a
Wedge129 <wedge0129@gmail.com>
2023-09-25 11:47:45 -0400

    Exercice Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if(num>0){
+            stringTemp=string;
+        for(let i=0;i<num-1;i++){
+            
+            string+=stringTemp;
+            }
+            return string;
+    }else if(num<0){
+        return string="ERROR"
+    }
+    else{
+        return string='';
+    }
+    
+    
 };
 
 // Do not edit below this line
