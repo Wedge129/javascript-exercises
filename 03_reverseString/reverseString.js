121ccbcb4867728c14e84da1609503460eb6b17c
Wedge129 <wedge0129@gmail.com>
2023-09-25 12:13:17 -0400

    Exercice Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    const stringArray = [];
+    const size = string.length;
+    for (let i = 0; i < size; i++) {
 
+        let char = string.charAt(i);
+        stringArray.push(char);
+    }
+    const stringArrayReversed = stringArray.reverse();
+    const finalString = stringArrayReversed.join("");
+    return finalString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
