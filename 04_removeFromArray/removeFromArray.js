61115d0e78a4fe211fcbde9a075cac7fc5f87355
Wedge129 <wedge0129@gmail.com>
2023-09-25 12:49:15 -0400

    First test completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, whatToRemove) {
+    const size=array.length;
+    for(let i =0;i<size;i++){
+        if(array[i]==whatToRemove){
+            array.splice(i,1);
+        }
+      //  console.log(array[i]);
+    }
+    //console.log(array.length);
+    return array;
 };
 
+//removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
