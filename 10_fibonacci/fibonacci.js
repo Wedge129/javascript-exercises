919b589f756b36e9f548d134552190f3fcd6d224
Wedge129 <wedge0129@gmail.com>
2023-10-05 10:10:01 -0400

    Exercice completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (fibonacciNumber) {
+    let a = 0, b = 1, c, i;
+    if(typeof(fibonacciNumber)=="string"){
+        parseInt(fibonacciNumber);
+    }
+    if(fibonacciNumber <0){
+        return "OOPS";
+    }
+    if( fibonacciNumber == 0)
+        return a;
+    for(i = 2; i <= fibonacciNumber; i++)
+    {
+    c = a + b;
+    a = b;
+    b = c;
+    }
+    return b;
 };
-
+//console.log(fibonacci(4));
 // Do not edit below this line
 module.exports = fibonacci;
