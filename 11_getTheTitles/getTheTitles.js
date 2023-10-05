ccc559867b9a62b6deb47547f79033159a925c65
Wedge129 <wedge0129@gmail.com>
2023-10-05 10:49:47 -0400

    Completed Exercice

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+const getTheTitles = function(arrayOfBooks) {
 
+    arrayOfTitles= arrayOfBooks.map(getTitles);
+
+    function getTitles(item){
+        //console.log(item.title);
+        return[item.title];
+    }
+    return arrayOfTitles;
+   
 };
-
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ]
+// getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
